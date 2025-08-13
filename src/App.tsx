9908b09241d9c1a4abb0910f625580cdb6c5a32a
Cris300390 <crisescalan300390@gmail.com>
2025-08-13 11:23:38 +0200

    feat: estructura inicial con Header y App limpia

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Bienvenido</p>
+    </div>
+  );
+}
+
+export default App;
