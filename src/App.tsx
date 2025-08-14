c3762b72fe284fe7834ce30fea45a31d1a484622
LauraGL <laugranadolopez@gmail.com>
2025-08-14 11:47:10 +0200

    Añadida navegación básica con react-router-dom y estructura inicial de vistas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import Header from "./components/Header";
-import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import UserPanel from './pages/UserPanel';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <p>Bienvenido</p>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/panel" element={<UserPanel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
